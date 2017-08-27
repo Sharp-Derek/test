f519f908aeff74ea527df2bc0f5d85fdac69d7b3
Sharp-Derek <thedereksharp@gmail.com>
2017-08-26 18:03:26 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Test</title>
+  </head>
+  <body>
+    <h1>Testing testing testing</h1>
+    <?php
+    include("test.html");
+    ?>
+  </body>
+</html>
